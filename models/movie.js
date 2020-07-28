d22641771239982f15894ce4f50d5b8b1b2f4b3e
julianortega <julianortega@MacBook-de-julianortega.local>
2020-07-28 19:48:50 +0200

    not quite done

---
+++ models/movie.js
@@ -0,0 +1,24 @@
+
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema(
+    {
+        title: String,
+        director: String,
+        stars: [String],
+        image: String,
+        descritpion: String,
+        showtimes: [String],
+
+
+    },
+    {
+      timestamps: true
+    }
+  );
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
+
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,42 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+const Movie = require('../models/movie.js')
 
-module.exports = router;
+
+/* GET home page */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Ironhack Cinema' });
+  });
+
+
+
+
+//iteración 3
+
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+      .then(allTheMoviesFromDB => {
+        console.log('Retrieved movies from DB:', allTheMoviesFromDB);
+        res.render('movies', {movies: allTheMoviesFromDB });
+      })
+      .catch(error => {
+        console.log('Error while getting the movies from the DB: ', error);
+      })
+  });
+
+
+//Iteración 4
+
+router.get('/movies/:moviesId', (req, res, next) => {
+    Movie.findById(req.params.moviesId)
+      .then(theMovie => {
+        res.render('/movie-details', { movies: theMovie});
+      })
+      .catch(error => {
+        console.log('Error while retrieving movies details: ', error);
+      })
+  });
+
+
+  module.exports = router;
